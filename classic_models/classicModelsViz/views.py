a7f6bbb81e75f46fd2c76eb09dae45256364cb51
Joao Henrique <joaohscorrea@gmail.com>
2024-05-01 15:25:37 -0300

    Adição do django paginator na listagens dos itens

--- classic_models/classicModelsViz/views.py
+++ classic_models/classicModelsViz/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from .models import *
 
 def index(request):
     customers = Customers.objects.all()
-    
+    paginator = Paginator(customers, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context ={
-        'customers':customers,
+        'page_obj': page_obj,
     }
     return render(request, 'index.html', context)
